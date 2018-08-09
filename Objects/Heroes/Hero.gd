fa4d549789e96145324132abee1f920251aafaea
Jonathan Picques <jonathan.picques@gmail.com>
2018-08-09 20:43:44 +0200

    feat(hero): improve slope movement

--- Objects/Heroes/Hero.gd
+++ Objects/Heroes/Hero.gd
@@ -13,6 +13,7 @@ enum HeroState {
 	fall,
 	fall_to_stand,
 }
+
 enum HeroDirection {
 	left = -1,
 	right = 1
@@ -22,8 +23,11 @@ const FLOOR_VECTOR = Vector3(0, 1, 0)
 const FLOOR_MAX_ANGLE = cos(deg2rad(45))
 const MOVE_STATE_THRESHOLD = 0.1
 
+# number of jumps (reset when grounded)
 var jumps = 0
-var coins = 0
+# amount of gold.
+var gold = 0
+# number of health points
 var health = 100
 
 var state = HeroState.stand
@@ -38,21 +42,9 @@ var input_velocity = Vector3()
 
 var direction = HeroDirection.right
 
-var on_wall = false
-var on_floor = false
-var on_ceiling = false
-var _on_wall_threshold = 0
-var _on_floor_threshold = 0
-var _on_ceiling_threshold = 0
-
-var floor_angle = 0
-var floor_velocity = Vector3()
-
-var _every_timer_tag = null
-
-
 onready var timer = $Timer
 onready var sprite = $HeroSprite
+onready var health_hud = $HudViewport/HealthHud
 onready var every_timer = $EveryTimer
 onready var walk_particles = $WalkParticles
 onready var animation_player = $AnimationPlayer
@@ -74,54 +66,10 @@ func _process(delta):
 # @impure
 # @param(float) delta
 func process_velocity(delta):
-	var old_translation = translation
-	velocity_prev = velocity
-	velocity = process_move_slide(delta)
-	var offset = translation - old_translation
-	velocity_offset = Vector3(
-		0 if is_nearly(offset.x, 0, 0.001) else velocity.x,
-		0 if is_nearly(offset.y, 0, 0.001) else velocity.y,
-		0 if is_nearly(offset.z, 0, 0.001) else velocity.z
-	)
+	pass
 
-# process_move_slide updates position after velocity and collisions are applied.
-# @impure
-# @param(float) delta
-func process_move_slide(delta):
-	on_wall = _on_wall_threshold > 0
-	on_floor = _on_floor_threshold > 0
-	on_ceiling = _on_ceiling_threshold > 0
-	_on_wall_threshold -= delta
-	_on_floor_threshold -= delta
-	_on_ceiling_threshold -= delta
-	floor_angle = 0
-	floor_velocity = Vector3()
-	var motion = (velocity + floor_velocity) * delta
-	var linear_velocity = velocity
-	for number_of_slide in range(0, 4):
-		var collision = move_and_collide(motion)
-		if collision:
-			motion = collision.remainder
-			if collision.normal.dot(FLOOR_VECTOR) >= FLOOR_MAX_ANGLE:
-				_on_floor_threshold = MOVE_STATE_THRESHOLD
-				floor_angle = collision.normal.angle_to(FLOOR_VECTOR)
-				floor_velocity = collision.collider_velocity
-				var relative_velocity = linear_velocity - floor_velocity
-				var horizontal_velocity = relative_velocity - FLOOR_VECTOR * FLOOR_VECTOR.dot(relative_velocity)
-				if collision.travel.length() < 0.05 and horizontal_velocity.length() < 0.001:
-					global_transform.origin -= collision.travel
-					return floor_velocity - FLOOR_VECTOR * FLOOR_VECTOR.dot(floor_velocity)
-			elif collision.normal.dot(-FLOOR_VECTOR) < FLOOR_MAX_ANGLE:
-				_on_wall_threshold = MOVE_STATE_THRESHOLD
-			else:
-				_on_ceiling_threshold = MOVE_STATE_THRESHOLD
-			motion = motion.slide(collision.normal)
-			linear_velocity = linear_velocity.slide(collision.normal)
-			if motion == Vector3():
-				break
-		else:
-			break
-	return linear_velocity
+## Timer
+## ---
 
 # start_timer starts a timer for the given duration in seconds.
 # @impure
@@ -130,23 +78,14 @@ func start_timer(duration):
 	timer.wait_time = duration
 	timer.start()
 
-# every_seconds returns true every given seconds.
-# @impure
-# @param(float) seconds
-# @param(string) timer_tag
-func every_seconds(seconds, timer_tag):
-	if _every_timer_tag != timer_tag or every_timer.is_stopped():
-		_every_timer_tag = timer_tag
-		every_timer.wait_time = seconds
-		every_timer.start()
-		return true
-	return false
-
 # is_timer_finished returns true if the timer is finished.
 # @pure
 # @param(float) duration
 func is_timer_finished():
 	return timer.is_stopped()
+
+## Animation / Direction
+## ---
 
 # change_direction changes the hero direction and flips the sprite accordingly.
 # @impure
@@ -160,7 +99,7 @@ func change_direction(new_direction):
 # @impure
 # @param(string) animation
 func change_animation(animation):
-	animation_player.play(animation + " Flashlight" if animation != "Turn" else "Turn")
+	animation_player.play(animation)
 
 # change_animation_speed changes the sprite animation speed.
 # @impure
@@ -168,11 +107,14 @@ func change_animation(animation):
 func change_animation_speed(speed):
 	animation_player.playback_speed = speed
 
+## Audio
+## ---
+
 # play_sound_effect plays a sound effect.
 # @impure
 # @param(AudioStreamSample) stream
 func play_sound_effect(stream):
-	var sound_effect_player = get_available_sound_effect_player()
+	var sound_effect_player = get_sound_effect_player()
 	if sound_effect_player != null:
 		sound_effect_player.stream = stream
 		sound_effect_player.play()
@@ -187,87 +129,42 @@ func is_sound_effect_playing(stream):
 			return true
 	return false
 
-# get_available_sound_effect_player returns the next available (non-playing) audio stream for sound effects or null.
+# get_sound_effect_player returns the next available (non-playing) audio stream for sound effects or null.
 # @pure
 # @returns(AudioStreamPlayer3D)
-func get_available_sound_effect_player():
+func get_sound_effect_player():
 	for sound_effect_player in sound_effect_players:
 		if not sound_effect_player.is_playing():
 			return sound_effect_player
 	return null
 
-# is_nearly returns true if the first given value nearly equals the second given value.
-# @pure
-# @param(float) value1
-# @param(float) value2
-# @param(float) epsilon
-# @returns(bool)
-func is_nearly(value1, value2, epsilon = 0.01):
-	return abs(value1 - value2) < epsilon
+## Gold / Health
+## ---
 
-# is_moving_x returns true if the given velocity has a non-zero x.
-# @pure
-# @param(Vector3) velocity
-# @returns(bool)
-func is_moving_x(velocity):
-	return velocity.x != 0
+# add_gold adds the given number of gold and updates the HUD.
+# @impure
+# @param(float) gold_to_add
+func add_gold(gold_to_add):
+	change_gold(gold + gold_to_add)
 
-# is_moving_z returns true if the given velocity has a non-zero z.
-# @pure
-# @param(Vector3) velocity
-# @returns(bool)
-func is_moving_z(velocity):
-	return velocity.z != 0
+# change_gold changes the given number of gold and updates the HUD.
+# @impure
+# @param(float) gold_to_change
+func change_gold(gold_to_change):
+	gold = max(0, gold_to_change)
+	health_hud.gold_counter = gold
 
-# is_moving_direction returns true if the given velocity in x is moving in the given direction.
-# @pure
-# @param (float) direction
-# @param(Vector3) velocity
-# @returns(bool)
-func is_moving_direction(direction, velocity):
-	return int(sign(velocity.x)) == direction
+# add_health adds the given number of health points and updates the HUD.
+# @impure
+# @param(float) health_to_add
+func add_health(health_to_add):
+	change_health(health + health_to_add)
 
-# get_movement returns the next velocity after given acceleration or deceleration is applied depending on the given direction.
-# @pure
-# @param(float) delta
-# @param(Vector3) velocity
-# @param(Vector3) direction
-# @param(Vector3) acceleration
-# @param(Vector3) maximum_speed
-# @returns(Vector3)
-func get_movement(delta, velocity, direction, acceleration, deceleration, maximum_speed, force = false):
-	return Vector3(
-		get_acceleration(delta, velocity.x, direction.x, acceleration.x, maximum_speed.x) if force or (velocity.x == 0 or sign(direction.x) == sign(velocity.x)) else get_deceleration(delta, velocity.x, deceleration.x),
-		velocity.y,
-		get_acceleration(delta, velocity.z, direction.z, acceleration.z, maximum_speed.z) if force or (velocity.z == 0 or sign(direction.z) == sign(velocity.z)) else get_deceleration(delta, velocity.z, deceleration.z)
-	)
-
-# get_acceleration returns the next value after the given acceleration is applied, clamped to the given maximum_speed.
-# @pure
-# @param(float) delta
-# @param(float) velocity
-# @param(float) direction
-# @param(float) acceleration
-# @param(float) maximum_speed
-# @returns(float)
-func get_acceleration(delta, velocity, direction, acceleration, maximum_speed):
-	return min(abs(velocity) + delta * acceleration, maximum_speed) * sign(direction)
-
-# get_deceleration returns the next value after the given deceleration is applied, clamped to zero.
-# @pure
-# @param(float) delta
-# @param(float) velocity
-# @param(float) deceleration
-# @returns(Vector3)
-func get_deceleration(delta, velocity, deceleration):
-	return max(abs(velocity) - delta * deceleration, 0) * sign(velocity)
-
-# get_gravity_acceleration returns the next value after the gravity is applied, clamped to the given gravity_max_speed.
-# @pure
-# @param(Vector3) delta
-# @param(Vector3) velocity
-# @param(Vector3) gravity
-# @param(Vector3) gravity_max_speed
-# @returns(Vector3)
-func get_gravity_acceleration(delta, velocity, gravity, gravity_max_speed):
-	return Vector3(velocity.x, max(velocity.y + gravity.y * delta, gravity_max_speed.y), velocity.z)
+# change_health changes the health points and updates the HUD.
+# @impure
+# @param(float) health_to_change
+func change_health(health_to_change):
+	health = health_to_change
+	health_hud.health_counter = health
+	if health <= 0:
+		pass # TODO: Death
--- Objects/Heroes/Luigi/Luigi.gd
+++ Objects/Heroes/Luigi/Luigi.gd
@@ -1,24 +1,5 @@
 extends "../Hero.gd"
 
-const MAX_JUMPS = 1
-const JUMP_STRENGTH = 5.5
-
-const GRAVITY = Vector3(0, -16.0, 0)
-const GRAVITY_MAX_SPEED = Vector3(0, -22.0, 0)
-
-const FLOOR_ACC = Vector3(4.0, 0.0, 4.0)
-const FLOOR_DEC = Vector3(6.0, 0.0, 4.5)
-const FLOOR_MAX_SPEED = Vector3(3.4, 0, 2.8)
-
-const AIRBORNE_ACC = Vector3(3.5, 0.0, 3.0)
-const AIRBORNE_DEC = Vector3(6.0, 0.0, 4.5)
-const AIRBORNE_MAX_SPEED = Vector3(3.2, 0.0, 2.4)
-
-var jump_sound = preload("./Sounds/Jump.ogg")
-var turn_sound = preload("./Sounds/Turn.ogg")
-var step_sound = preload("./Sounds/NormalStep.ogg")
-var coin_sound = preload("../../Pickups/Coin/Sounds/Coin.ogg")
-
 # Hero update loop
 # ---
 
@@ -58,156 +39,64 @@ func set_state(new_state):
 ## ---
 
 func pre_stand():
-	jumps = MAX_JUMPS
-	change_animation("Stand")
+	pass
 
 func stand(delta):
-	velocity = get_gravity_acceleration(delta, velocity, GRAVITY, GRAVITY_MAX_SPEED)
-	if not on_floor:
-		return set_state(HeroState.fall)
-	elif input_jump and jumps > 0:
-		return set_state(HeroState.jump)
-	elif is_moving_x(input_velocity) and not is_moving_direction(direction, input_velocity):
-		return set_state(HeroState.walk_turn)
-	elif is_moving_x(input_velocity) or is_moving_z(input_velocity):
-		return set_state(HeroState.walk)
-	elif is_moving_x(velocity) or is_moving_z(velocity):
-		return set_state(HeroState.walk_skid)
-
-var walk_lock_velocity = 0
-var walk_lock_direction = 0
+	pass
 
 func pre_walk():
-	walk_particles.emitting = true
-	walk_lock_velocity = input_velocity
-	walk_lock_direction = direction
-	start_timer(0.25)
-	every_seconds(1.0, "r")
-	change_animation("Walk")
-	if is_moving_direction(direction, velocity) and abs(velocity.x) > FLOOR_MAX_SPEED.x - 1.0:
-		return self.set_state(HeroState.walk_2)
-	change_animation_speed(1.6)
+	pass
 
 func walk(delta):
-	velocity = get_movement(delta, velocity, walk_lock_velocity, FLOOR_ACC, FLOOR_DEC, FLOOR_MAX_SPEED)
-	velocity = get_gravity_acceleration(delta, velocity, GRAVITY, GRAVITY_MAX_SPEED)
-	if every_seconds(0.08, "walk"):
-		play_sound_effect(step_sound)
-	if not on_floor:
-		change_animation_speed(1.0)
-		walk_particles.emitting = false
-		return set_state(HeroState.fall)
-	elif input_jump and jumps > 0:
-		walk_particles.emitting = false
-		return set_state(HeroState.jump)
-	elif is_timer_finished():
-		change_animation_speed(1.0)
-		return self.set_state(HeroState.walk_2)
+	pass
 
 func pre_walk_2():
-	every_seconds(1.0, "r")
+	pass
 
 func walk_2(delta):
-	velocity = get_movement(delta, velocity, input_velocity, FLOOR_ACC, FLOOR_DEC, FLOOR_MAX_SPEED)
-	velocity = get_gravity_acceleration(delta, velocity, GRAVITY, GRAVITY_MAX_SPEED)
-	if every_seconds(0.24, "walk"):
-		play_sound_effect(step_sound)
-	if not on_floor:
-		walk_particles.emitting = false
-		return set_state(HeroState.fall)
-	elif on_wall and not is_moving_x(velocity_offset) and not is_moving_z(velocity_offset):
-		walk_particles.emitting = false
-		return set_state(HeroState.walk_wall)
-	elif input_jump and jumps > 0:
-		walk_particles.emitting = false
-		return set_state(HeroState.jump)
-	elif is_moving_direction(-walk_lock_direction, input_velocity):
-		walk_particles.emitting = false
-		return self.set_state(HeroState.walk_turn)
-	elif not is_moving_x(input_velocity) and not is_moving_z(input_velocity):
-		walk_particles.emitting = false
-		return self.set_state(HeroState.walk_skid)
+	pass
 
 func pre_walk_wall():
-	change_animation("Stand")
+	pass
 
 func walk_wall(delta):
-	velocity = get_movement(delta, velocity, input_velocity, FLOOR_ACC, FLOOR_DEC, FLOOR_MAX_SPEED)
-	velocity = get_gravity_acceleration(delta, velocity, GRAVITY, GRAVITY_MAX_SPEED)
-	if not on_floor:
-		return set_state(HeroState.fall)
-	elif not on_wall or is_moving_x(velocity_offset) or is_moving_z(velocity_offset):
-		return set_state(HeroState.stand)
-	elif input_jump and jumps > 0:
-		return set_state(HeroState.jump)
+	pass
 
 func pre_walk_skid():
-	change_animation("Skid")
+	pass
 
 func walk_skid(delta):
-	velocity = get_movement(delta, velocity, Vector3(), FLOOR_ACC, FLOOR_DEC if not is_moving_direction(-walk_lock_direction, input_velocity) else FLOOR_ACC + FLOOR_DEC, FLOOR_MAX_SPEED)
-	velocity = get_gravity_acceleration(delta, velocity, GRAVITY, GRAVITY_MAX_SPEED)
-	if not on_floor:
-		return set_state(HeroState.fall)
-	elif input_jump and jumps > 0:
-		return set_state(HeroState.jump)
-	elif is_moving_x(input_velocity) and not is_moving_direction(direction, input_velocity):
-		return set_state(HeroState.walk_turn)
-	elif is_moving_x(input_velocity) or is_moving_z(input_velocity):
-		return set_state(HeroState.walk)
-	elif not is_moving_x(velocity) and not is_moving_z(velocity):
-		return set_state(HeroState.stand)
+	pass
 
 func pre_walk_turn():
-	start_timer(0.1)
-	change_animation("Turn")
-	play_sound_effect(turn_sound)
+	pass
 
 func walk_turn(delta):
-	velocity = get_movement(delta, velocity, Vector3(), FLOOR_ACC, FLOOR_ACC + FLOOR_DEC, FLOOR_MAX_SPEED)
-	velocity = get_gravity_acceleration(delta, velocity, GRAVITY, GRAVITY_MAX_SPEED)
-	if is_timer_finished():
-		change_direction(-direction)
-		return self.set_state(HeroState.stand)
+	pass
 
 ## Vertical movement states
 ## ---
 
 func pre_jump():
-	jumps -= 1
-	velocity = Vector3(velocity.x, JUMP_STRENGTH, velocity.z)
-	change_animation("Jump")
-	play_sound_effect(jump_sound)
+	pass
 
 func jump(delta):
-	if velocity.y < 0:
-		set_state(HeroState.fall)
-	velocity = get_movement(delta, velocity, input_velocity, AIRBORNE_ACC, AIRBORNE_DEC, AIRBORNE_MAX_SPEED)
-	velocity = get_gravity_acceleration(delta, velocity, GRAVITY, GRAVITY_MAX_SPEED)
+	pass
 
 func pre_fall():
-	change_animation("Fall")
+	pass
 
 func fall(delta):
-	velocity = get_movement(delta, velocity, input_velocity, AIRBORNE_ACC, AIRBORNE_DEC, AIRBORNE_MAX_SPEED)
-	velocity = get_gravity_acceleration(delta, velocity, GRAVITY, GRAVITY_MAX_SPEED)
-	if on_floor:
-		return set_state(HeroState.fall_to_stand)
+	pass
 
 func pre_fall_to_stand():
-	start_timer(0.08)
-	change_animation("Skid")
-	play_sound_effect(step_sound)
+	pass
 
 func fall_to_stand(delta):
-	velocity = get_movement(delta, velocity, Vector3(), FLOOR_ACC, FLOOR_DEC, FLOOR_MAX_SPEED)
-	velocity = get_gravity_acceleration(delta, velocity, GRAVITY, GRAVITY_MAX_SPEED)
-	if is_timer_finished():
-		return self.set_state(HeroState.stand)
+	pass
+
+## Pickup
+## ---
 
 func _on_AreaPickup_body_entered(body):
-	if body.is_in_group("Coin"):
-		coins += 10
-		body.queue_free()
-		play_sound_effect(coin_sound)
-		$HudViewport/HealthHud.set_gold_counter(coins)
+	pass
